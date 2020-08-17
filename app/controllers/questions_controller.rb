3454bba80f2ab06f6f4f2c14ac995f9b1425670d
Daan Roelofs <daanroelofs599@gmail.com>
2020-08-17 15:18:01 +0200

    Added Stupid Coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params['question'] == 'I am going to work'
+      puts @answer = 'Great!'
+    elsif params['question'].include?('?')
+      puts @answer = 'Silly question, get dressed and go to work!'
+    else
+      puts @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
